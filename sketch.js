665c931c9ab252af89c3533b8ede7e1edc4621c6
krishakapoor06 <68777962+krishakapoor06@users.noreply.github.com>
2020-08-25 17:18:56 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -40,7 +40,7 @@ function draw() {
 function hasCollided (bullet,wall)  {
   bulletRightEdge = bullet.x + bullet.width;
   wallLeftEdge = wall.x;
-  if (bulletRightEdge === wallLeftEdge) {
+  if (bulletRightEdge >= wallLeftEdge) {
 return true
   }
   return false
